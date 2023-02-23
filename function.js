1f4eb81a779b2b4d7e62f73a9d65944d7aa18bb4
Jsiejk <123989344+Jsiejk@users.noreply.github.com>
2023-02-23 09:37:58 -0700

    Add files via upload
    Implemented a toggle for dark/light modes. Need to make a fix, though, as clicking to generate a new joke resets the color back to default. Began adding a way to choose between generating 1-4 joke/memes. Only the drop down bar has been added so far, as I need to research it more.

--- function.js
+++ function.js
@@ -1,3 +1,26 @@
+const colorswitch = document.getElementById('colorswitch');
+const options = document.getElementById('options');
+const content = document.getElementById('content');
+const jokeformat = document.getElementById('joke');
+
+colorswitch.addEventListener('change',e => {
+    const isChecked = e.target.checked;
+
+    if(isChecked) {
+        options.classList.add('swap');
+        content.classList.add('swap');
+        jokeformat.classList.add('swap');
+    }
+    else {
+        options.classList.remove('swap');
+        content.classList.remove('swap');
+        jokeformat.classList.remove('swap');
+    }
+        
+})
+
+
+
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://api.chucknorris.io/jokes/random";
