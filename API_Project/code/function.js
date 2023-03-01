ebbf2b0e7d7931395a78cfb82aba1d7c42115d1b
Jsiejk <123989344+Jsiejk@users.noreply.github.com>
2023-03-01 13:23:47 -0700

    Add files via upload

--- API_Project/code/function.js
+++ API_Project/code/function.js
@@ -1,12 +1,14 @@
 const colorswitch = document.getElementById('colorswitch');
 const options = document.getElementById('options');
 const content = document.getElementById('content');
-const jokeformat = document.getElementById('joke');
+const jokeformat = document.getElementById('joke','joke2','joke3','joke4');
+const jokeContainer = document.getElementById("joke","joke2","joke3","joke4");
+const btn = document.getElementById("btn");
 
-colorswitch.addEventListener('change',e => {
+colorswitch.addEventListener('change', e => {
     const isChecked = e.target.checked;
-
-    if(isChecked) {
+    
+    if (isChecked) {
         options.classList.add('swap');
         content.classList.add('swap');
         jokeformat.classList.add('swap');
@@ -15,33 +17,93 @@ colorswitch.addEventListener('change',e => {
         options.classList.remove('swap');
         content.classList.remove('swap');
         jokeformat.classList.remove('swap');
-
     }
-        
+    
 })
 
+/**
+ * @param {Array<T>|string} arr 
+ * @return {T|string}
+*/
+const getRandom = (arr) => {
+    if (arr.length) {
+        const idx = Math.floor(Math.random() * arr.length);
+        return arr[idx]
+    } else {
+        return null;
+    }
+};
 
 
 
-const jokeContainer = document.getElementById("joke");
-const btn = document.getElementById("btn");
-const url = "https://api.chucknorris.io/jokes/random";
 
-let getJoke = (e) => {
+const getJoke = async (e) => {
+    const userSearch = document.getElementById("search-text").value;
+    let url = 'https://api.chucknorris.io/jokes/search?query=Chuck';
+
+    if (userSearch) {
+        url = `https://api.chucknorris.io/jokes/search?query=${userSearch}`;
+    }
+    
+    // Prevent the webpage from reloading on submit
     if (e) {
         e.preventDefault();
     }
     
-    jokeContainer.classList.remove("fade");
-    fetch(url)
-    .then(data => data.json())
-    .then(item =>{
-        console.log(item);
-        jokeContainer.textContent = `${item.value}`;
-        jokeContainer.classList.add("fade");
-    });
+    // jokeContainer.classList.remove("fade");
+    
+    // Calling the url to get the joke
+    const response = await fetch(url);
+
+    // Finding value of selected radio and changing layout accordingly
+    if (document.getElementById('num1id').checked) {
+
+        
+        // if you get a 200-399
+        if (response.ok) {
+            const data = await response.json(); // object
+            const dataArr = data?.result; // array
+            const filtered = dataArr?.filter((v) => { // filtered for explicit
+                // ternary
+                // return v?.categories.indexOf('explicit') >= 0 ? false : true;
+                
+                // same thing
+                // const cats = v?.categories;
+                // if (cats.indexOf('explicit') >= 0) {
+                    // return false;
+                    // } else {
+                        //  return true;
+                        // }
+                        
+                        // with stringify
+                        // return JSON.stringify(v).indexOf('explicit') < 0;
+            
+                        // with regex (best)
+                        const reggie = /explicit|fuck|dick|mom/gi
+                        const string = JSON.stringify(v);
+                        const match = string.match(reggie);
+                        return match == null ? true : false;
+                    });
+                    
+                    // take out the explicit jokes
+                    if (filtered && filtered.length) { /// TODO: check for the right number of jokes
+                        const item = getRandom(filtered);
+                        jokeContainer.textContent = `${item.value}`;
+                    }
+                    
+                    
+                    else {
+                        console.error('Not enough jokes!')
+                        jokeContainer.textContent = "Not enough jokes. Please try again!"
+                    }
+                } else {
+                    jokeContainer.textContent = "Please try a new search. There is a 3 character minimum, and only real words will give a result."
+    }
+}
+else if(document.getElementById('num2id').checked) {
+
+    
+}
 }
 
-btn.addEventListener("click",getJoke);
-getJoke();
-
+btn.addEventListener("click", getJoke);
