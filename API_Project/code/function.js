a88752ff7581bd0fa28f58181fd650038b85a8ea
Jsiejk <123989344+Jsiejk@users.noreply.github.com>
2023-03-02 10:41:16 -0700

    Add files via upload

--- API_Project/code/function.js
+++ API_Project/code/function.js
@@ -1,13 +1,19 @@
 const colorswitch = document.getElementById('colorswitch');
 const options = document.getElementById('options');
 const content = document.getElementById('content');
-const jokeformat = document.getElementById('joke','joke2','joke3','joke4');
-const jokeContainer = document.getElementById("joke","joke2","joke3","joke4");
+let jokeformat = document.getElementById('joke1');
+let jokeContainer = document.getElementById("joke1");
 const btn = document.getElementById("btn");
+
+console.log(document.querySelector('input[name="jokeNumber"]'));
+// const form = document.getElementById("form");
+// const formData = new FormData(form);
+// console.log(formData);
+// console.log(numofJokes);
 
 colorswitch.addEventListener('change', e => {
     const isChecked = e.target.checked;
-    
+
     if (isChecked) {
         options.classList.add('swap');
         content.classList.add('swap');
@@ -18,7 +24,7 @@ colorswitch.addEventListener('change', e => {
         content.classList.remove('swap');
         jokeformat.classList.remove('swap');
     }
-    
+
 })
 
 /**
@@ -37,6 +43,8 @@ const getRandom = (arr) => {
 
 
 
+
+
 const getJoke = async (e) => {
     const userSearch = document.getElementById("search-text").value;
     let url = 'https://api.chucknorris.io/jokes/search?query=Chuck';
@@ -44,66 +52,69 @@ const getJoke = async (e) => {
     if (userSearch) {
         url = `https://api.chucknorris.io/jokes/search?query=${userSearch}`;
     }
-    
+
     // Prevent the webpage from reloading on submit
     if (e) {
         e.preventDefault();
     }
-    
+
     // jokeContainer.classList.remove("fade");
-    
+
     // Calling the url to get the joke
     const response = await fetch(url);
 
-    // Finding value of selected radio and changing layout accordingly
-    if (document.getElementById('num1id').checked) {
 
-        
-        // if you get a 200-399
-        if (response.ok) {
-            const data = await response.json(); // object
-            const dataArr = data?.result; // array
-            const filtered = dataArr?.filter((v) => { // filtered for explicit
-                // ternary
-                // return v?.categories.indexOf('explicit') >= 0 ? false : true;
-                
-                // same thing
-                // const cats = v?.categories;
-                // if (cats.indexOf('explicit') >= 0) {
-                    // return false;
-                    // } else {
-                        //  return true;
-                        // }
-                        
-                        // with stringify
-                        // return JSON.stringify(v).indexOf('explicit') < 0;
-            
-                        // with regex (best)
-                        const reggie = /explicit|fuck|dick|mom/gi
-                        const string = JSON.stringify(v);
-                        const match = string.match(reggie);
-                        return match == null ? true : false;
-                    });
-                    
-                    // take out the explicit jokes
-                    if (filtered && filtered.length) { /// TODO: check for the right number of jokes
-                        const item = getRandom(filtered);
-                        jokeContainer.textContent = `${item.value}`;
-                    }
-                    
-                    
-                    else {
-                        console.error('Not enough jokes!')
-                        jokeContainer.textContent = "Not enough jokes. Please try again!"
-                    }
-                } else {
-                    jokeContainer.textContent = "Please try a new search. There is a 3 character minimum, and only real words will give a result."
+
+
+    // if you get a 200-399
+    if (response.ok) {
+        const data = await response.json(); // object
+        const dataArr = data?.result; // array
+        const filtered = dataArr?.filter((v) => { // filtered for explicit
+            // ternary
+            // return v?.categories.indexOf('explicit') >= 0 ? false : true;
+
+            // same thing
+            // const cats = v?.categories;
+            // if (cats.indexOf('explicit') >= 0) {
+            // return false;
+            // } else {
+            //  return true;
+            // }
+
+            // with stringify
+            // return JSON.stringify(v).indexOf('explicit') < 0;
+
+            // with regex (best)
+            const reggie = /explicit|fuck|dick|mom/gi
+            const string = JSON.stringify(v);
+            const match = string.match(reggie);
+            return match == null ? true : false;
+        });
+
+        let numofJokes = document.querySelector('input[name="jokeNumber"]:checked')?.value;
+        console.log(numofJokes);
+
+        // if they asked for more jokes than there are, don't do anything
+        if (filtered?.length < numofJokes) {
+            console.error('Not enough jokes!')
+            jokeContainer.textContent = "Not enough jokes. Please try again!"
+        }
+        else {
+            for (let i = 0; i < numofJokes; i++) {
+                jokeContainer = document.getElementById(`joke${i + 1}`);
+                console.log(jokeContainer);
+                const item = getRandom(filtered);
+                console.log(item);
+                jokeContainer.textContent = `${item.value}`;
+                // filtered.remove(item);
+            }
+        }
+
+
     }
 }
-else if(document.getElementById('num2id').checked) {
 
-    
-}
-}
+
 
 btn.addEventListener("click", getJoke);
